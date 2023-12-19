fa9e9ca98bc2a8605aa97acc7523670accb40931
Danilo Cerne <prog.danilocerne@gmail.com>
2023-12-19 02:43:55 -0300

    Adds EntityFramework MySQL dependencies in the Infra.Data layer and creates ApplicationDbContewhich is responsible for interacting with the MySQL database.

---
+++ BPOneTestAPI/BPOneTestAPI.Infra.Data/Context/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using BPOneTestAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BPOneTestAPI.Infra.Data.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDBContext).Assembly);
+        }
+    }
+}
