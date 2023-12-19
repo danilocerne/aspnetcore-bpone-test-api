c5ce6c63048cca9be7c77f2e0e34a3dc2bbc59d2
Danilo Cerne <prog.danilocerne@gmail.com>
2023-12-19 07:23:26 -0300

    In the Infra.IoC layer, create a static class responsible for adding the ApplicationDbContext (database instance) to the IServiceCollection service that AspNet Core uses to inject dependencies into its Dependency Manager Container.

---
+++ BPOneTestAPI/BPOneTestAPI.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using BPOneTestAPI.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BPOneTestAPI.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructureMySql(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("DataBase_BPOneTestAPI");
+            services.AddDbContext<ApplicationDbContext>(
+
+                options => options.UseMySql(connection,
+                ServerVersion.AutoDetect(connection),
+                mySqlOptions =>
+                {
+                    mySqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 10,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null);
+
+                    mySqlOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+                }
+                )
+
+             );
+
+            return services;
+        }
+    }
+}
